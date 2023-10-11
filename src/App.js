715d2799c8ff09246565b62030429eb7cb67ae35
CoderPavlo <pavlo4060@gmail.com>
2023-10-11 15:46:05 +0300

    implemented start page

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import {BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar"
+import StartPage from "./pages/StartPage/StartPage";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+function App() {
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#007BFF', // Синій (Primary Color)
+      },
+      secondary: {
+        main: '#CCCCCC', // Сріблястий (Secondary Color)
+      },
+      background: {
+        default: '#333333', // Темно-сірий (Background Color)
+      },
+      error: {
+        main: '#FF5733', // Червоний (Accent Color)
+      },
+      success: {
+        main: '#28A745', // Зелений (Positive Action Color)
+      },
+      warning: {
+        main: '#FFC107', // Помаранчевий (Warning Color)
+      },
+      text: {
+        primary: '#FFFFFF', // Білий (Text Color)
+      },
+      common: {
+        white: '#FFFFFF', // Білий (Background Color)
+      },
+    },
+  });
+
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<StartPage />} />
+
+
+
+            
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/context/LanguageContext.js
@@ -0,0 +1,17 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const LanguageContext = createContext();
+
+export function LanguageProvider({ children }) {
+  const [language, setLanguage] = useState('uk'); // Початкова мова - українська
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+}
+
+export function useLanguage() {
+  return useContext(LanguageContext);
+}
